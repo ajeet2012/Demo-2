f1c95b493f4155a8eb5ff11beae293973eab1cd6
ajeet2012 <ajeetmca2012@gmail.com>
2025-11-02 15:13:28 +0400

    Initial commit = Demo 2

---
+++ Demo-2/Demo-2/Demo_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_2App.swift
+//  Demo-2
+//
+//  Created by Ajeet Sharma on 02/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Demo_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
